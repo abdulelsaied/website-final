042e31146cffe19e3c9954dc6df01704e5df3c62
Abdul El-Saied <abdule@Abduls-MacBook-Air.local>
2021-07-22 18:55:25 -0700

    color + content update

--- typewriter.js
+++ typewriter.js
@@ -11,7 +11,7 @@ var TxtType = function(el, toRotate, period, tickCount) {
 
     TxtType.prototype.tick = function() {
         this.tickCount++;
-        if (this.tickCount === 174) {
+        if (this.tickCount === 154) {
           return;
         }
         var i = this.loopNum % this.toRotate.length;
